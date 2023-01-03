a3a64bd54a8816b06785fa239495eac4cd3b33bc
IsmailHosen25 <ismailhosen1006@gmail.com>
2023-01-03 13:51:19 +0600

    navigation var done

---
+++ home.js
@@ -0,0 +1,28 @@
+// -------------------------------------------Navigation var Start--------------------------------------------------------- //
+ let logo_img=document.getElementById("logo-img"); // id="logo-img"
+ logo_img.addEventListener("click",function(){
+    let url="home.html";
+    window.open(url,"_self")
+ })
+
+let logo_head=document.getElementById("logo-head"); // id="logo-head"
+logo_head.addEventListener("click",function(){
+    let url="home.html";
+    window.open(url,"_self")
+ })
+
+let manu_icon=document.getElementById('manu-icon');  // id="manu-icon
+manu_icon.addEventListener("click",function(){
+    let ul=document.getElementById('ul-res'); // id="ul-res"
+    ul.classList.toggle("ul-respons");
+})
+
+// ///////////////////// This is for Products ul
+// function prod_ul(){
+//     let prod_ul=document.getElementById("prod-ul") //id='prod-ul'
+//     prod_ul.classList.toggle("prod-active");
+// }
+//////////////////////
+
+
+// -------------------------------------------Navigation var End--------------------------------------------------------- //
